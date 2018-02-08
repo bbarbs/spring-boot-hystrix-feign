bdeb539ec685b1203d3c14b02c1a4bd5a11856e2
bbarbs <barberobrainold@gmail.com>
2018-02-08 15:44:23 +0800

    Remove json extraction process in exception

--- gateway/src/main/java/com/gateway/exception/handler/ApiExceptionHandler.java
+++ gateway/src/main/java/com/gateway/exception/handler/ApiExceptionHandler.java
@@ -5,8 +5,6 @@ import com.gateway.exception.ApiException;
 import com.gateway.exception.CustomerConflictException;
 import com.gateway.exception.CustomerNotFoundException;
 import com.gateway.exception.message.ApiExceptionMessage;
-import com.gateway.util.TextUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,9 +15,6 @@ import java.io.IOException;
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
-    @Autowired
-    TextUtil textUtil;
-
     private ObjectMapper objectMapper = new ObjectMapper();
 
     /**
@@ -28,9 +23,8 @@ public class ApiExceptionHandler {
      * @param e
      * @return
      */
-    private ApiExceptionMessage getExceptionMessage(ApiException e) throws IOException {
-        String errorMessage = this.textUtil.extractJson(e.getLocalizedMessage(), 2);
-        return this.objectMapper.readValue(errorMessage, ApiExceptionMessage.class);
+    private ApiExceptionMessage readException(ApiException e) throws IOException {
+        return this.objectMapper.readValue(e.getMessage(), ApiExceptionMessage.class);
     }
 
     /**
@@ -42,7 +36,7 @@ public class ApiExceptionHandler {
      */
     @ExceptionHandler(CustomerNotFoundException.class)
     public ResponseEntity<ApiExceptionMessage> customerNotFoundException(ApiException e) throws IOException {
-        return new ResponseEntity<>(getExceptionMessage(e), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(this.readException(e), HttpStatus.NOT_FOUND);
     }
 
     /**
@@ -54,6 +48,6 @@ public class ApiExceptionHandler {
      */
     @ExceptionHandler(CustomerConflictException.class)
     public ResponseEntity<ApiExceptionMessage> customerConflictException(ApiException e) throws IOException {
-        return new ResponseEntity<>(getExceptionMessage(e), HttpStatus.CONFLICT);
+        return new ResponseEntity<>(this.readException(e), HttpStatus.CONFLICT);
     }
 }
